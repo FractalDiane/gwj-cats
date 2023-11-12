f7f29d3d6a8b020daeaa6aa5cafd7768e5a4b05c
Diane Sparks <duncan.sprks@gmail.com>
2023-11-11 21:52:17 -0800

    paws message on paws chair

--- scripts/managers/level_manager.gd
+++ scripts/managers/level_manager.gd
@@ -3,6 +3,7 @@ extends Node
 var current_level: Level = null
 
 var sources_pausing_time := 0
+var pause_text: Node2D = null
 
 @onready var level_timer := $LevelTimer as Timer
 
@@ -13,6 +14,10 @@ func start_level(level: Level) -> void:
 func push_time_pause_source() -> void:
 	sources_pausing_time += 1
 	level_timer.paused = true
+	pause_text = preload("res://scenes/pawsed_text.tscn").instantiate() as Node2D
+	pause_text.position = Vector2(960, 540)
+	pause_text.scale = Vector2(0.5, 0.5)
+	HUD.add_hud_child(pause_text)
 	
 	
 func pop_time_pause_source() -> void:
@@ -20,3 +25,4 @@ func pop_time_pause_source() -> void:
 		sources_pausing_time -= 1
 		if sources_pausing_time == 0:
 			level_timer.paused = false
+			pause_text.queue_free()

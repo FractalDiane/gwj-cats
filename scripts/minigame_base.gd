586f791c5b2926279d65ed79c7bd7cbe47489b6e
Calli Kiler <sfkiler94@gmail.com>
2023-11-12 15:40:06 -0800

    integrated lever and ticket minigames, added overhead for lever state while minigame open
    minigames can now receive an early return request signal passed through the minigame object and from the task manager, by default it just returns (false, 0) but for the train lever minigame it will return whatever lever direction data it would return if the minigame was closed

--- scripts/minigame_base.gd
+++ scripts/minigame_base.gd
@@ -1,7 +1,9 @@
 class_name Minigame
-extends Node
+extends Node2D
 
 signal minigame_done(success: bool, data: int)
+
+signal early_return_send(success: bool, data: int)
 
 func exit_task() -> void:
 	minigame_done.emit(false, 0)
@@ -10,3 +12,8 @@ func _input(event):
 	if event is InputEventKey:
 		if event.is_action_pressed("exit"):
 			exit_task()
+
+func early_return_requested():
+	# by default, return (false, 0) if data is requested before minigame has closed
+	# (train control minigame is only one so far that can have returnable data before minigame is closed)
+	early_return_send.emit(false, 0)

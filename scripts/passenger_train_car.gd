2d0b2d808bf342b28df1aecca4c230293a7c8061
notpresident35 <notpresident35@gmail.com>
2023-11-19 06:48:28 -0800

    resolve interaction bugs

--- scripts/passenger_train_car.gd
+++ scripts/passenger_train_car.gd
@@ -2,10 +2,10 @@ extends TrainCar
 
 @export var anim: AnimationPlayer
 @export var fade_time: float = 1.5
-@export var passenger_possibilities: Array
 
 func _ready():
-	for i in range(2):
-		var p = get_node(passenger_possibilities.pick_random())
-		p.set_process(true)
-		p.set_visible(true)
+	while available_minigames.size() > 2:
+		var i = randi_range(0, available_minigames.size()-1)
+		#n.get_parent().remove_child(n)
+		available_minigames[i].queue_free()
+		available_minigames.remove_at(i)
